22ec4a9d13a82c70d0ee6fe9e4d23f0ce530172f
Souleimane <souleimane.ch.ahmed@gmail.com>
2025-12-07 23:00:09 +0300

    adding styling for the dashboard

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Enable Whitenoise storage
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
